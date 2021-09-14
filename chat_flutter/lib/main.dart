8b6e14ac87cc80ee77a21f3e324837e4b7a3870c
Haku <brancher81@gmail.com>
2021-09-13 22:06:01 -0300

    First commit of this project just with parametrization of firebase

---
+++ chat_flutter/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container(
+            child: Text("Outro erro"),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          FirebaseFirestore.instance.collection("col").doc("doc").set({"texto": "haku 1"});
+          return Container();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container(
+          child: Text("Erro"),
+        );
+      },
+    );
+  }
+}
