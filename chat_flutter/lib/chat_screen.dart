c26611d03362ff035787f35e8e3005d0b451ebb8
Haku <brancher81@gmail.com>
2021-09-14 23:25:07 -0300

    Implemented send function

--- chat_flutter/lib/chat_screen.dart
+++ chat_flutter/lib/chat_screen.dart
@@ -1,14 +1,18 @@
 import 'package:chat_flutter/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({Key? key}) : super(key: key);
-
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  void _sendMessage(String text){
+    FirebaseFirestore.instance.collection("messages").add({"text" : text});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +20,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("Olá"),
         elevation: 0,
       ),
-      body: TextComposer(),
+      body: TextComposer(_sendMessage),
     );
   }
 }
