949c569e3a9e9561734790fb30d88a11e9536d3d
Haku <brancher81@gmail.com>
2021-09-16 20:03:53 -0300

    Created google authentication and recording user data on Firebase

--- chat_flutter/lib/chat_screen.dart
+++ chat_flutter/lib/chat_screen.dart
@@ -29,7 +29,7 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   Future<User?> _getUser() async{
-    
+
     try {
       final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
       final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount!.authentication;
